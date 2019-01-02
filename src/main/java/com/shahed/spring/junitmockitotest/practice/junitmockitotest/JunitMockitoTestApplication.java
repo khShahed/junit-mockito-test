b6972fd2090d1c2bd5508eb35aac5a5e20ff273b
khshahed <khshahed@users.noreply.github.com>
2019-01-02 12:27:46 +0600

    Initial commit

---
+++ src/main/java/com/shahed/spring/junitmockitotest/practice/junitmockitotest/JunitMockitoTestApplication.java
@@ -0,0 +1,14 @@
+package com.shahed.spring.junitmockitotest.practice.junitmockitotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMockitoTestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/shahed/spring/junitmockitotest/practice/junitmockitotest/JunitMockitoTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.shahed.spring.junitmockitotest.practice.junitmockitotest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JunitMockitoTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
